c54dfe66ee09cd14c148a743b175fa97ea76bd5e
Marina Stakhovets <marina.stakhovets@gmail.com>
2025-02-24 14:23:37 +0300

    create migrations

--- weather_project/weather/models.py
+++ weather_project/weather/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Weather(models.Model):
+    city = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    temperature = models.FloatField()
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.city} - {self.temperature}°C'
